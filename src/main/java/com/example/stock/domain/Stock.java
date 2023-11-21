39f1352873a890f75b4ce37d4b53a619d19b9063
mykim8710 <mykim8710@gmail.com>
2023-11-21 13:47:05 +0900

    [ dev ] #1 - 재고감소 로직 개발 : Stock entity 개발

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,26 @@
+package com.example.stock.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long itemId;
+    private Long quantity;
+
+    @Builder
+    public Stock(Long itemId, Long quantity) {
+        this.itemId = itemId;
+        this.quantity = quantity;
+    }
+}
