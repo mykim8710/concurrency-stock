b0f40d38b1e6601ca4256a8e7ae2677ce30c09fd
mykim8710 <mykim8710@gmail.com>
2023-11-21 14:38:51 +0900

    [ dev ] #1 - 재고감소 로직 개발 : 재고로직 테스트 케이스 추가 - 동시에 여러 쓰레드에서 요청을 할때의 테스트케이스 추가(실패케이스)

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -9,6 +9,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @SpringBootTest
@@ -40,6 +45,38 @@ class StockServiceTest {
         // then
         Stock findStock = stockRepository.findById(id).get();
         Assertions.assertThat(findStock.getQuantity()).isEqualTo(99);
+    }
+
+    @Test
+    @DisplayName("[실패] 동시에 100개의 요청이 들어올때 재고감소 테스트")
+    void 재고감소_동시100개요청_실패_테스트() throws Exception {
+        // given
+        int threadCount = 100;
+
+        // ExecutorService : 비동기로 실행하는 작업을 단순화하여 사용할 수 있게 도와주는 java api
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // CountDownLatch : 다른 스레드에서 수행 중인 작업이 완료될 때까지 대기할수 있도록 도와주는 클래스
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        // when
+        for(int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decreaseStock(1L, 1L);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        // then
+        Stock findStock = stockRepository.findById(1L).get();
+        Assertions.assertThat(findStock.getQuantity()).isEqualTo(0);
     }
 
 }
